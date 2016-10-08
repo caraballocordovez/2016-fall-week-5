7c8afc540d476c644cee90a9793e3795cb8b34bd
Jorge Caraballo Cordovez <jorgecaraballocordovez@MacBook-Pro-de-Jorge.local>
2016-10-08 14:24:17 -0400

    work in class

--- 5.2/script.js
+++ 5.2/script.js
@@ -13,4 +13,31 @@ var plot = d3.select('.canvas').append('svg')
 
 //Import data...
 
-console.log('Import completed');
+/* d3.csv("../data/data.csv", parse, function(error, rows){
+	console.log(error);
+	console.table(rows);
+
+});
+
+
+function parse(d){
+	console.log(d);
+
+	return{
+		x: +d.x,
+		y: +d.y,
+		r: +d.r
+	}
+} */
+
+d3.csv("../data/data.csv", parse, function(error, rows){
+	console.table(rows);
+}
+);
+
+function parse(d){
+	return{
+		x: (+d.x)?(+d.x):undefined // This is called a ternary operation: (condition)?a:b. It's read like "If the condition (in parenthesis) is true, return a; if it's false, return b. In this case what we're saying is "if d.x is a string, return undefined; if it's a number, return the number."
+		y: +(d.y.slice(2)) //Starts the string from the third character of what was a string. 
+	}
+}
--- 5.3/script.js
+++ 5.3/script.js
@@ -12,8 +12,42 @@ var plot = d3.select('.canvas').append('svg')
     .attr('transform','translate('+ m.l+','+ m.t+')');
 
 //Import data...
+d3.csv("../data/world_bank_2013.csv", parse, function(error, rows){
+	console.table(rows);
+
+	var mingdpPerCap = d3.min(rows, function(d) {return d.gdpPerCap; }),
+		maxgdpPerCap = d3.max(rows, function(d) {return d.gdpPerCap; });
+
+		console.log(mingdpPerCap, maxgdpPerCap);
+
+	var scaleX = d3.scaleLinear(),
+		scaleY = d3.scaleLinear();
+
+	scaleY.domain([0,100])
+		.range([h,0]);
+
+	scaleX.domain([mingdpPerCap, maxgdpPerCap])
+		.range([0,w]);
+
+	rows.forEach(function(country){
+		plot.append("circle")
+			.attr("cx", scaleX(country.gdpPerCap))
+			.attr("cy", scaleY(country.user))
+			.attr("r", 5)
+			.style("fill-opacity", .7);
+	})
+
+});
 
 //Write parse function
+function parse(d){
+/*return an object*/	
+return {
+	country: d.country,
+	gdpPerCap: (+d.GDP_per_capita)?(+d.GDP_per_capita):undefined,
+	user: (+d.internet_users_per_100)?(+d.internet_users_per_100):undefined,
+}
+}
 
 //Mining for max and min
 
